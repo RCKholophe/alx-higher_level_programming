9cfa870b644089f98200bcb7b0c52751388e137e
RCKholophe <khongelani8@gmail.com>
2023-11-30 11:51:03 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,13 @@
+#!/usr/bin/python3
+
 def magic_calculation(a, b):
-    add, sub = mai=gic_calculation_102.add, magic_calculation_102.sub
+    """Match bytecode provided by ALX."""
+    from magic_calculation_102 import add, sub
 
-    if a<b:
-        c=add(a,b)
-
+    if a < b:
+        c = add(a,b)
         for i in range(4, 6):
-            c=add(c, i)
-
+            c = add(c, i)
             return c
-
-        return sub(a, b)
+        else:
+            return(sub(a, b))

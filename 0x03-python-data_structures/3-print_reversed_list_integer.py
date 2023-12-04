61efaac6eee89f073856208e88ec40e6b82303ca
RCKholophe <khongelani8@gmail.com>
2023-12-04 13:10:56 -0800

    3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+ # -----------------------------------------------------------
+ # Python program that:
+ # demonstrates how to print all integers of a list, in reverse order
+ # (C) 2023 Kholophe Rabelani,South Africa,Johannesburg
+ # email khongelani8@gmail.com
+ # -----------------------------------------------------------
+
+ def print_reversed_list_integer(my_list=[]):
+     """Prints all integers of a list, in reverse order
+
+     Args:
+         my_list: a list
+     """
+
+     if my_list:
+         my_list.reverse()
+         for item in my_list:
+             print("{:d}".format(item))

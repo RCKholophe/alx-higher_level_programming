c425a028aa7235f60950e929903d6c900f4ff83e
RCKholophe <khongelani8@gmail.com>
2023-12-04 14:08:34 -0800

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,33 +1,10 @@
 #!/usr/bin/python3
- # -----------------------------------------------------------
- # Python program that:
- # demonstrates how to print a matrix of integers
- #
- # (C) 2023 Kholophe Rabelani,Johanneaburg, South Africa
- # email khongelani8@gmail.com
- # -----------------------------------------------------------
-
-
  def print_matrix_integer(matrix=[[]]):
-     """Prints a matrix of integers
-
-     Args:
-         matrix: a matrix
-     """
-
-     # If no argument is supplied print a newline
      if not matrix:
-         print()
-     else:
-         # Loop through the rows
-         for row in matrix:
-             # Loop through the items in row
-             for item in row:
-                 # Add space between items if not last item in row
-                 if row.index(item) != len(row) - 1:
-                     endspace = " "
-                 else:
-                     endspace = ""
-                 # Print the items in the row
-                 print("{:d}".format(item), end=endspace)
+         return None
+     for submatrix in matrix:
+         if len(submatrix) == 0:
              print()
+         for i in range(len(submatrix)):
+             print("{:d}".format(submatrix[i]),
+                     end="\n" if i is len(submatrix) - 1 else " ")
